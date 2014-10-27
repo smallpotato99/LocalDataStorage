da43207b66ff94b493281e8ffa86907a2a48a205
hacker1202 <hacker1202@hotmail.com>
2014-10-27 10:29:26 +0800

    preference activity example

--- src/com/example/localdatastorage/MainActivity.java
+++ src/com/example/localdatastorage/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.localdatastorage;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
@@ -10,16 +12,17 @@ import android.view.View;
 public class MainActivity extends Activity {
 
 	public final static String LOGTAG="EXPLORECA";
-	public final static String USERNAME="username";
+	public final static String USERNAME="pref_username";
+	public final static String VIEWIMAGES="pref_viewimages";
 	
 	private SharedPreferences settings;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main2);
 		
-		settings = getPreferences(MODE_PRIVATE);
+		settings = PreferenceManager.getDefaultSharedPreferences(this);
 	}
 
 	@Override
@@ -44,11 +47,16 @@ public class MainActivity extends Activity {
 	public void setPreference (View v) {
 		Log.i(LOGTAG, "Click set");
 		
+		/* Code for Java pref
 		SharedPreferences.Editor editor = settings.edit();
 		String prefValue = UIHelper.getText(this, R.id.editText1);
 		editor.putString(USERNAME, prefValue);
 		editor.commit();
 		UIHelper.displayText(this, R.id.textView1, "Preference saved");
+		*/
+		
+		Intent intent = new Intent(this, SettingsActivity.class);
+		startActivity(intent);
 	}
 	
 	public void refreshDisplay (View v) {
@@ -56,5 +64,6 @@ public class MainActivity extends Activity {
 		
 		String prefValue = settings.getString(USERNAME, "Not found");
 		UIHelper.displayText(this, R.id.textView1, prefValue);
+		UIHelper.setCBChecked(this, R.id.checkBox1, settings.getBoolean(VIEWIMAGES, false));
 	}
 }
