54430283662f0bd5344636e4cffab69c2238095b
smallpotato99 <hacker1202@hotmail.com>
2014-10-27 22:59:50 +0800

    tidy up

--- gen/com/example/localdatastorage/R.java
+++ gen/com/example/localdatastorage/R.java
@@ -1201,9 +1201,10 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_intfile=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_main2=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_json=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_main2=0x7f03001b;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
     }
     public static final class menu {
         public static final int main=0x7f0d0000;
